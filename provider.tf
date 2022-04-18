0f806ed89a753f53d5fb055e8efb85de26b5b2e5
Kacper Szczepanek <kacper.szczepanek@nordcloud.com>
2022-04-19 00:33:44 +0200

    params for cicd

--- provider.tf
+++ provider.tf
@@ -6,12 +6,10 @@ terraform {
     key            = "terraform.tfstate"
     region         = "eu-west-1"
     dynamodb_table = "eng-project-infra-dynamo"
-    profile        = "kacper"
   }
 }
 
 provider "aws" {
   region  = "eu-west-1"
-  profile = "kacper"
 }
 
