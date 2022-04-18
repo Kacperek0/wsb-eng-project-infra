cb0e1524812b35421855107437a98f446b123c42
Kacper Szczepanek <kacper.szczepanek@nordcloud.com>
2022-04-19 00:12:03 +0200

    setup remote backend

--- provider.tf
+++ provider.tf
@@ -1,8 +1,17 @@
 terraform {
     required_version = ">= 1.1.8"
+
+    backend "s3" {
+        bucket = "eng-project-infra"
+        key    = "terraform.tfstate"
+        region = "eu-west-1"
+        dynamodb_table = "eng-project-infra-dynamo"
+        profile = "kacper"
+    }
 }
 
 provider "aws" {
   region = "eu-west-1"
   profile = "kacper"
 }
+
