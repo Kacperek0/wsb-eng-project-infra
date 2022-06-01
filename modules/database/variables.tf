decde6f02b418a16ae3ff78489db717ff9c6ebd8
Kacperek0 <kacper.szczepanek@gmail.com>
2022-06-01 22:36:06 +0200

    db

---
+++ modules/database/variables.tf
@@ -0,0 +1,9 @@
+variable "cluster_identifier" {
+  description = "The name of the RDS cluster"
+  type        = string
+}
+
+variable "db_name" {
+  description = "The name of the database"
+  type        = string
+}
