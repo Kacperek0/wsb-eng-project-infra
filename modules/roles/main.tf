21769730fbe5b8716d5e20c49f214687e24e11f1
Kacperek0 <47487512+Kacperek0@users.noreply.github.com>
2022-06-18 14:08:48 +0200

    store to s3 (#14)

--- modules/roles/main.tf
+++ modules/roles/main.tf
@@ -46,10 +46,22 @@ resource "aws_iam_policy" "lambda_logging" {
       Action = [
         "logs:CreateLogGroup",
         "logs:CreateLogStream",
-        "logs:PutLogEvents"
+        "logs:PutLogEvents",
+        "s3:GetBucketLocation",
+        "s3:GetObject",
+        "s3:ListBucket",
+        "s3:ListBucketMultipartUploads",
+        "s3:PutObject",
+        "s3:AbortMultipartUpload",
+        "s3:DeleteObject",
+        "s3:GetBucketPolicy",
+        "s3:GetBucketTagging",
+        "s3:GetObjectVersion",
+        "s3:GetBucketVersioning"
       ]
       Resource = [
-        "arn:aws:logs:*:*:*"
+        "arn:aws:logs:*:*:*",
+        "arn:aws:s3:::*"
       ]
     }]
   })
