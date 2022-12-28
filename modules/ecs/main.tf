4f8f519e4d36a4719cd54335d3c9041df6101d6c
Kacperek0 <kacper.szczepanek@gmail.com>
2022-12-28 17:30:14 +0100

    fix

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -1,9 +1,3 @@
-resource "aws_kms_key" "key" {
-  description             = "KMS key for log encryption"
-  deletion_window_in_days = 7
-
-  tags = var.tags
-}
 
 resource "aws_cloudwatch_log_group" "log_group" {
   name              = var.log_group_name
@@ -18,18 +12,6 @@ resource "aws_ecs_cluster" "cluster" {
   setting {
     name  = "containerInsights"
     value = "enabled"
-  }
-
-  configuration {
-    execute_command_configuration {
-      kms_key_id = aws_kms_key.key.arn
-      logging    = "OVERRIDE"
-
-      log_configuration {
-        cloud_watch_encryption_enabled = true
-        cloud_watch_log_group          = aws_cloudwatch_log_group.log_group.name
-      }
-    }
   }
 
   tags = var.tags
