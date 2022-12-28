205e47ad99b3c9c18008222a5564dd7aed5182d9
Kacperek0 <kacper.szczepanek@gmail.com>
2022-12-28 17:28:06 +0100

    fix

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -26,8 +26,8 @@ resource "aws_ecs_cluster" "cluster" {
       logging    = "OVERRIDE"
 
       log_configuration {
-        cloudwatch_encryption_enabled = true
-        cloudwatch_log_group          = aws_cloudwatch_log_group.log_group.name
+        cloud_watch_encryption_enabled = true
+        cloud_watch_log_group          = aws_cloudwatch_log_group.log_group.name
       }
     }
   }
